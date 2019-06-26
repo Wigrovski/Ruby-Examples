2c18e1d8aa733848339f1aecd66b6b48a87b86e4
Wigrovski <wigrovski@mail.ru>
2019-06-26 18:19:42 +0700

    Программа time вычисляет дни между двумя датами

---
+++ time.rb
@@ -0,0 +1,9 @@
+require 'date'
+puts 'введите начальную дату: '
+s = gets.chomp
+puts 'введите конечную дату: '
+e = gets.chomp
+start_date = Date.parse(s)
+end_date = Date.parse(e)
+day = (start_date...end_date).count.to_s
+puts 'Колличество дней: ' + day
