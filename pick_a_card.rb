5d9ca85c27effa66f998abf2fbc23d2629d434d1
Wigrovski <wigrovski@mail.ru>
2019-07-06 00:55:39 +0700

    update pick a card

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -1,5 +1,5 @@
 values = ['2','3','4','5','6','7','8','9','10','J','Q','K','A']
-suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades'] # Буби, Черви, Крести, Пики
+suits = ['♦', '♥', '♣', '♠'] # Буби, Черви, Крести, Пики
 
 # Выведем произвольную карты, выбрав по одному элементу из массивов
-puts "#{values.sample} of #{suits.sample}"
+puts "#{values.sample} #{suits.sample}"
